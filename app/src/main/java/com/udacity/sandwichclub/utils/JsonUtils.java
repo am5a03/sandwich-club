c5d4a42a12fe486844fa6a920b08548f2a0601d7
Raymond Chan <raymond@9gag.com>
2019-02-14 10:41:31 +0800

    Init code

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,26 @@
 package com.udacity.sandwichclub.utils;
 
+import android.util.JsonReader;
+import android.util.Log;
+
 import com.udacity.sandwichclub.model.Sandwich;
+
+import java.io.StringReader;
 
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
+        final StringReader stringReader = new StringReader(json);
+        final JsonReader jsonReader = new JsonReader(stringReader);
+
+        final Sandwich sandwich = new Sandwich();
+
+        try {
+            jsonReader.nextString();
+        } catch (Exception e) {
+            Log.e(JsonUtils.class.getSimpleName(), "parseSandwichJson: ", e);
+        }
+
         return null;
     }
 }
